625bfcde261e8f342b14e41542494732786979f8
Lindsey Olson <lindseyolson00@gmail.com>
2017-09-02 10:02:51 -0500

    hamming solution

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,10 @@
+class Hamming
+  def self.compute(strand1, strand2)
+    raise ArgumentError unless strand1.length == strand2.length
+      strand1.chars.zip(strand2.chars).count{ |a, b| a != b }
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
