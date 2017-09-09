5fd7a9730bf985aba1a99f909c66e481f8e856b9
Lindsey Olson <lindseyolson00@gmail.com>
2017-09-09 17:53:43 -0500

    completed gigasecond and updated JS rna-transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,27 +1,27 @@
-var DnaTranscriber = function(dna) {
-  this.dna = dna;
-};
+var DnaTranscriber = function() {};
 
-DnaTranscriber.prototype.toRna = function() {
-  let dna = this.dna.split('');
-  for (var i = 0; i < dna.length; i++) {
-    switch(dna[i]) {
-      case 'G':
-        dna[i] = 'C';
-        break;
-      case 'C':
-        dna[i] = 'G';
-        break;
-      case 'T':
-        dna[i] = 'A';
-        break;
-      case 'A':
-        dna[i] = 'U';
-        break;
+DnaTranscriber.prototype.toRna = function(dna_string) {
+  let dna = dna_string.split('');
+    for (var i = 0; i < dna.length; i++) {
+      switch(dna[i]) {
+        case 'G':
+          dna[i] = 'C';
+          break;
+        case 'C':
+          dna[i] = 'G';
+          break;
+        case 'T':
+          dna[i] = 'A';
+          break;
+        case 'A':
+          dna[i] = 'U';
+          break;
+        default:
+          throw Error('Invalid input');
+      }
     }
-  }
   return dna.join('');
 };
 
-var dna = new DnaTranscriber('GCTA');
-dna.toRna();
+
+module.exports = DnaTranscriber;
